a940ce7ca824dfd15061a599598e4a688e360410
gaby <125655898+gamerblack@users.noreply.github.com>
2023-11-16 08:40:11 -0300

    Add files via upload

---
+++ assets/scripts/script.js
@@ -0,0 +1,9 @@
+ function abrirmenu() {
+    let menu = document.getElementById("menu");
+    
+    if (menu.style.display === "none"){
+        menu.style.display = "block"
+    } else{
+        menu.style.display = "none"
+    }
+ }
